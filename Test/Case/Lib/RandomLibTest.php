2a8afb176f3f8811325852c4fe29dc9f5e15dd57
euromark <euromark@web.de>
2012-08-01 00:32:12 +0200

    random lib

---
+++ Test/Case/Lib/RandomLibTest.php
@@ -0,0 +1,55 @@
+<?php
+
+App::uses('RandomLib', 'Tools.Lib');
+
+class RandomLibTest extends CakeTestCase {
+
+
+	public function testInt() {
+		$is = RandomLib::int(2, 200);
+		pr($is);
+		$this->assertTrue($is >= 2 && $is <= 200);
+
+	}
+
+
+	public function testArrayValue() {
+		$array = array(
+			'x',
+			'y',
+			'z',
+		);
+		$is = RandomLib::arrayValue($array, null, null, true);
+		pr($is);
+		$this->assertTrue(in_array($is, $array));
+
+		# non-numerical indexes
+		$array = array(
+			'e' => 'x',
+			'f' => 'y',
+			'g' => 'z',
+		);
+		$is = RandomLib::arrayValue($array);
+		pr($is);
+		$this->assertTrue(in_array($is, $array));
+
+	}
+
+
+	public function testPronounceablePwd() {
+		$is = RandomLib::pronounceablePwd(6);
+		pr($is);
+		$this->assertTrue(strlen($is) === 6);
+
+		$is = RandomLib::pronounceablePwd(11);
+		pr($is);
+		$this->assertTrue(strlen($is) === 11);
+	}
+
+
+
+	//TOOD: other tests
+
+
+}
+
