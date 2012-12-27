e044441eef7f304d2b18c17d70c08677adc17743
euromark <euromark@web.de>
2012-12-27 15:49:52 +0100

    MyController init

---
+++ Controller/MyController.php
@@ -0,0 +1,44 @@
+<?php
+App::uses('Controller', 'Controller');
+
+/**
+ * DRY Controller stuff
+ * 2011-02-01 ms
+ */
+class MyController extends Controller {
+
+	/**
+	 * Fix for asset compress to not run into fatal error loops
+	 * 2012-12-25 ms
+	 */
+	public function __construct($request = null, $response = null) {
+  	parent::__construct($request, $response);
+		if (strpos($this->here, '/js/cjs/') === 0 || strpos($this->here, '/css/ccss/') === 0) {
+			unset($this->request->params['ext']);
+		}
+	}
+
+	/**
+	 * Add headers for IE8 etc to fix caching issues in those stupid browsers
+	 * 2012-12-25 ms
+	 */
+	public function disableCache() {
+		$this->response->header(array(
+			'Pragma' => 'no-cache',
+		));
+		return parent::disableCache();
+	}
+
+	/**
+	 * Init Packages class if enabled/included
+	 * @deprecated?
+	 * 2012-12-25 ms
+	 */
+	public function beforeRender() {
+		if (class_exists('Packages')) {
+			Packages::initialize($this, __CLASS__);
+		}
+		parent::beforeRender();
+	}
+
+}
