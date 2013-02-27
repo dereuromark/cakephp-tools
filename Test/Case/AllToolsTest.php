0b6b2aaa066efdb5952173b84fbef26e4598ca02
euromark <euromark@web.de>
2013-02-27 11:30:04 +0100

    AllTools test

---
+++ Test/Case/AllToolsTest.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * group test - Tools
+ */
+class AllAuthTestsTest extends PHPUnit_Framework_TestSuite {
+
+	/**
+	 * suite method, defines tests for this suite.
+	 *
+	 * @return void
+	 */
+	public static function suite() {
+		$Suite = new CakeTestSuite('All Auth tests');
+		$path = dirname(__FILE__);
+		$Suite->addTestDirectory($path . DS . 'Controller' . DS . 'Component' . DS . 'Auth');
+		return $Suite;
+	}
+}
