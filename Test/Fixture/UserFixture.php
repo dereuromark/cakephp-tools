0d673b76edc0a08e37d7b6a1abf5fcda611ef6cc
euromark <euromark@web.de>
2013-06-15 02:12:09 +0200

    UserFixture

---
+++ Test/Fixture/UserFixture.php
@@ -0,0 +1,32 @@
+<?php
+
+/**
+ * User Fixture
+ */
+class UserFixture extends CakeTestFixture {
+
+	/**
+	 * Fields
+	 *
+	 * @var array
+	 */
+	public $fields = array(
+		'id' => array('type' => 'integer', 'key' => 'primary'),
+		'name' => array('type' => 'string', 'null' => false),
+		'password' => array('type' => 'string', 'null' => false),
+		'role_id' => array('type' => 'integer', 'null' => false),
+	);
+
+	/**
+	 * records property
+	 *
+	 * @var array
+	 */
+	public $records = array(
+		array('id' => 1, 'role_id' => 1, 'password' => '123', 'name' => 'User 1'),
+		array('id' => 2, 'role_id' => 2, 'password' => '123', 'name' => 'User 2'),
+		array('id' => 3, 'role_id' => 1, 'password' => '123', 'name' => 'User 3'),
+		array('id' => 4, 'role_id' => 3, 'password' => '123', 'name' => 'User 4')
+	);
+
+}
