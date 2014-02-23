3431846284ec1c7b782812dbb2aa25074e1936f8
Augusto <augf87@gmail.com>
2014-02-23 15:09:22 -0300

    Version check fix

--- Model/Behavior/JsonableBehavior.php
+++ Model/Behavior/JsonableBehavior.php
@@ -175,7 +175,7 @@ class JsonableBehavior extends ModelBehavior {
 		}
 		if (is_array($val)) {
 			// $depth param added in php 5.4
-			if (version_compare(PHP_VERSION, '5.4.0', '>=')) {
+			if (version_compare(PHP_VERSION, '5.5.0', '>=')) {
 				$val = json_encode($val, $this->settings[$Model->alias]['encodeParams']['options'], $this->settings[$Model->alias]['encodeParams']['depth']);
 			} 
 			else {
