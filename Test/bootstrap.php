46711af51f77ffb709980be18aebdb4ffcc97b93
euromark <euromark@web.de>
2014-04-17 04:39:13 +0200

    bootstrap

--- Test/bootstrap.php
+++ Test/bootstrap.php
@@ -6,3 +6,25 @@
  * unit tests in this file.
  */
 //require dirname(__DIR__) . '/../Config/bootstrap.php';
+
+define('DS', DIRECTORY_SEPARATOR);
+define('ROOT', dirname(dirname(dirname(dirname(__FILE__)))));
+define('APP_DIR', 'App');
+define('WEBROOT_DIR', 'webroot');
+define('APP', ROOT . DS . APP_DIR . DS);
+define('WWW_ROOT', ROOT . DS . WEBROOT_DIR . DS);
+define('TESTS', ROOT . DS . 'Test' . DS);
+define('TMP', ROOT . DS . 'tmp' . DS);
+define('LOGS', TMP . 'logs' . DS);
+define('CACHE', TMP . 'cache' . DS);
+define('CAKE_CORE_INCLUDE_PATH', ROOT . '/vendor/cakephp/cakephp');
+define('CORE_PATH', CAKE_CORE_INCLUDE_PATH . DS);
+define('CAKE', CORE_PATH . 'src' . DS);
+
+require ROOT . '/vendor/cakephp/cakephp/src/basics.php';
+require ROOT . '/vendor/autoload.php';
+
+$TMP = new \Cake\Utility\Folder(TMP);
+$TMP->create(TMP . 'cache/models', 0777);
+$TMP->create(TMP . 'cache/persistent', 0777);
+$TMP->create(TMP . 'cache/views', 0777);
