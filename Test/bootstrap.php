f95a938e934356ff1d04b9c504cfa6cbb4e25e03
euromark <euromark@web.de>
2014-04-17 04:31:36 +0200

    remove init

--- Test/bootstrap.php
+++ Test/bootstrap.php
@@ -5,4 +5,4 @@
  * Add additional configuration/setup your application needs when running
  * unit tests in this file.
  */
-require dirname(__DIR__) . '/../Config/bootstrap.php';
+//require dirname(__DIR__) . '/../Config/bootstrap.php';
