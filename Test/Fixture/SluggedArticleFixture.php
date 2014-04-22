7ccfc28401f565d2e646e914bf240564dd99032e
euromark <euromark@web.de>
2014-04-22 23:21:25 +0200

    fix test

--- Test/Fixture/SluggedArticleFixture.php
+++ Test/Fixture/SluggedArticleFixture.php
@@ -17,10 +17,10 @@ class SluggedArticleFixture extends TestFixture {
  */
 	public $fields = array(
 		'id' => ['type' => 'integer'],
-		'title' => ['type' => 'string', 'length' => 255, 'null' => false],
-		'slug' => ['type' => 'string', 'length' => 245, 'null' => false],
-		'long_title' => array('type' => 'string', 'null' => false),
-		'long_slug' => array('type' => 'string', 'null' => false),
+		'title' => ['type' => 'string', 'length' => 255, 'null' => false, 'default' => ''],
+		'slug' => ['type' => 'string', 'length' => 245, 'null' => false, 'default' => ''],
+		'long_title' => array('type' => 'string', 'null' => false, 'default' => ''),
+		'long_slug' => array('type' => 'string', 'null' => false, 'default' => ''),
 		'section' => ['type' => 'integer', 'null' => true],
 		'_constraints' => ['primary' => ['type' => 'primary', 'columns' => ['id']]]
 	);
