be573ee676f20dd078eba6121731c9027d1bf2b1
euromark <euromark@web.de>
2014-09-30 04:38:05 +0200

    Correct namespace

--- tests/Fixture/AfterTreesFixture.php
+++ tests/Fixture/AfterTreesFixture.php
@@ -1,17 +1,5 @@
 <?php
-/**
- * CakePHP(tm) : Rapid Development Framework (http://cakephp.org)
- *
- * Licensed under The MIT License
- * For full copyright and license information, please see the LICENSE.txt
- * Redistributions of files must retain the above copyright notice.
- *
- * @copyright     Copyright (c) Cake Software Foundation, Inc. (http://cakefoundation.org)
- * @link          http://www.cakephp.org
- * @since         1.2.0
- * @license       http://www.opensource.org/licenses/mit-license.php MIT License
- */
-namespace Cake\Test\Fixture;
+namespace Tools\Test\Fixture;
 
 use Cake\TestSuite\Fixture\TestFixture;
 
