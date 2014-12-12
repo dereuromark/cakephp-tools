af62cb7aababa26a92f41ad1c241d148e83b367f
euromark <euromark@web.de>
2014-12-12 15:17:12 +0100

    Add test

---
+++ tests/TestCase/View/Helper/JsHelperTest.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace Tools\TestCase\View\Helper;
+
+use Tools\View\Helper\JsHelper;
+use Cake\TestSuite\TestCase;
+use Cake\View\View;
+use Cake\ORM\TableRegistry;
+use Cake\Datasource\ConnectionManager;
+use Cake\Core\Configure;
+
+class JsHelperTest extends TestCase {
+
+	/**
+	 * @return void
+	 */
+	public function setUp() {
+		parent::setUp();
+
+		$this->Js = new JsHelper(new View(null));
+	}
+
+	/**
+	 * @return void
+	 */
+	public function tearDown() {
+		unset($this->Table);
+
+ 		//TableRegistry::clear();
+		parent::tearDown();
+	}
+
+	/**
+	 * @return void
+	 */
+	public function testObject() {
+		$this->assertInstanceOf('Tools\View\Helper\JsHelper', $this->Js);
+	}
+
+	/**
+	 * JsHelperTest::testBuffer()
+	 *
+	 * @return void
+	 */
+	public function testBuffer() {
+		$script = <<<JS
+jQuery(document).ready(function() {
+	// Code
+});
+JS;
+		$this->Js->buffer($script);
+
+		$output = $this->Js->writeBuffer();
+
+		$expected = <<<HTML
+<script>
+//<![CDATA[
+jQuery(document).ready(function() {
+	// Code
+});
+//]]>
+</script>
+HTML;
+		$this->assertTextEquals($expected, $output);
+	}
+
+}
