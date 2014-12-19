1c9aa7f2f69d8aa33df5be4e680ef0c93fabeb5b
Marc Würth <ravage@bluewin.ch>
2014-12-19 16:27:25 +0100

    Typo

--- View/Helper/GoogleMapV3Helper.php
+++ View/Helper/GoogleMapV3Helper.php
@@ -27,7 +27,7 @@ App::uses('AppHelper', 'View/Helper');
  *
  * v1.4: Better handling of script output and directions added
  * You can now either keep map() + script(), or you can now write the script to the buffer with
- * map() + finalize(). You can then decide wether the JS should be in the head or the footer of your layout.
+ * map() + finalize(). You can then decide whether the JS should be in the head or the footer of your layout.
  * Don't forget to put `echo $this->Js->writeBuffer(array('inline' => true));` somewhere in your layout then, though.
  * You can now also add directions using addDirections().
  *
