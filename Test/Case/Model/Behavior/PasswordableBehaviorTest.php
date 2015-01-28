60b522adc00530bb788b1c46c790c6c23d4d931a
Mark Scherer <euromark@web.de>
2015-01-28 23:49:23 +0100

    Fix include

--- Test/Case/Model/Behavior/PasswordableBehaviorTest.php
+++ Test/Case/Model/Behavior/PasswordableBehaviorTest.php
@@ -4,7 +4,7 @@ App::uses('AuthComponent', 'Controller/Component');
 App::uses('SimplePasswordHasher', 'Controller/Component/Auth');
 
 if (!function_exists('password_hash')) {
-	require_once CakePlugin::path('Tools') . 'Lib/Bootstrap/Password.php';
+	require_once CakePlugin::path('Shim') . 'Lib/Bootstrap/Password.php';
 }
 
 class PasswordableBehaviorTest extends CakeTestCase {
