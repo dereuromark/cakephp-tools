313bd378a7cbbae69544af73ce757e7d550c387a
Mark Scherer <euromark@web.de>
2015-11-15 23:49:36 +0100

    Avoid inline assignment

--- src/Controller/Controller.php
+++ src/Controller/Controller.php
@@ -13,12 +13,14 @@ class Controller extends ShimController {
 	 * Handles automatic pagination of model records.
 	 *
 	 * @overwrite to support defaults like limit etc.
+	 *
 	 * @param \Cake\ORM\Table|string|\Cake\ORM\Query $object Table to paginate
 	 *   (e.g: Table instance, 'TableName' or a Query object)
 	 * @return \Cake\ORM\ResultSet Query results
 	 */
 	public function paginate($object = null) {
-		if ($defaultSettings = (array)Configure::read('Paginator')) {
+		$defaultSettings = (array)Configure::read('Paginator');
+		if ($defaultSettings) {
 			$this->paginate += $defaultSettings;
 		}
 		return parent::paginate($object);
