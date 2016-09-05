e709a188418c27a9c1dbe3cce77a47e74f5d84c5
Mark Sch <dereuromark@users.noreply.github.com>
2016-09-05 15:45:29 +0200

    Update routes.php

--- config/routes.php
+++ config/routes.php
@@ -1,6 +1,7 @@
 <?php
+use Cake\Routing\Route\DashedRoute;
 use Cake\Routing\Router;
 
 Router::plugin('Tools', function ($routes) {
-	$routes->fallbacks('DashedRoute');
+	$routes->fallbacks(DashedRoute::class);
 });
