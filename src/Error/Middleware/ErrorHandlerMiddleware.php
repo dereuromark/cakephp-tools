e0a789d75d245d413d3441e3225c28034d93af82
dereuromark <euromark@web.de>
2016-11-27 12:29:08 +0100

    Add ErrorHandlerMiddleware

--- src/Error/Middleware/ErrorHandlerMiddleware.php
+++ src/Error/Middleware/ErrorHandlerMiddleware.php
@@ -1,16 +1,19 @@
 <?php
 
-namespace Tools\Error;
+namespace Tools\Error\Middleware;
 
-use Cake\Error\ErrorHandler as CoreErrorHandler;
+use Cake\Core\Configure;
 use Cake\Log\Log;
 use Exception;
+use Cake\Error\Middleware\ErrorHandlerMiddleware as CoreErrorHandlerMiddleware;
 
 /**
+ * Error handling middleware.
+ *
  * Custom ErrorHandler to not mix the 404 exceptions with the rest of "real" errors in the error.log file.
  *
- * All you need to do is:
- * - switch `use Cake\Error\ErrorHandler;` with `use Tools\Error\ErrorHandler;` in your bootstrap
+ * All you need to do is in your Application.php file for `new ErrorHandlerMiddleware()`:
+ * - Switch `use Cake\Error\Middleware\ErrorHandlerMiddleware;` with `use Tools\Error\Middleware\ErrorHandlerMiddleware;`
  * - Make sure you got the 404 log defined either in your app.php or as Log::config() call.
  *
  * Example config as scoped one:
@@ -25,18 +28,33 @@ use Exception;
  *
  * In case you need custom 404 mappings for some additional custom exceptions, make use of `log404` option.
  * It will overwrite the current defaults completely.
- *
- * @deprecated As of CakePHP 3.3+. Use Tools\Error\Middleware\ErrorHandlerMiddleware now.
  */
-class ErrorHandler extends CoreErrorHandler {
+class ErrorHandlerMiddleware extends CoreErrorHandlerMiddleware
+{
 
 	/**
-	 * Handles exception logging
-	 *
-	 * @param \Exception $exception Exception instance.
-	 * @return bool
+	 * @param string|callable|null $renderer The renderer or class name
+	 *   to use or a callable factory.
+	 * @param array $config Configuration options to use. If empty, `Configure::read('Error')`
+	 *   will be used.
 	 */
-	protected function _logException(Exception $exception) {
+	public function __construct($renderer = null, array $config = [])
+	{
+		if ($renderer === null) {
+			$renderer = Configure::read('Error.exceptionRenderer');
+		}
+
+		parent::__construct($renderer, $config);
+	}
+
+    /**
+     * Log an error for the exception if applicable.
+     *
+     * @param \Psr\Http\Message\ServerRequestInterface $request The current request.
+     * @param \Exception $exception The exception to log a message for.
+     * @return void
+     */
+    protected function logException($request, $exception) {
 		$blacklist = [
 			'Cake\Routing\Exception\MissingControllerException',
 			'Cake\Routing\Exception\MissingActionException',
@@ -52,15 +70,16 @@ class ErrorHandler extends CoreErrorHandler {
 			'Cake\Network\Exception\UnauthorizedException',
 			'Cake\Network\Exception\NotAcceptableException',
 		];
-		if (isset($this->_options['log404'])) {
-			$blacklist = $this->_options['log404'];
+		if (isset($this->_config['log404'])) {
+			$blacklist = $this->_config['log404'];
 		}
 		if ($blacklist && in_array(get_class($exception), (array)$blacklist)) {
 			$level = LOG_ERR;
-			Log::write($level, $this->_getMessage($exception), ['404']);
-			return false;
+			Log::write($level, $this->getMessage($request, $exception), ['404']);
+			return;
 		}
-		return parent::_logException($exception);
-	}
+
+        parent::logException($request, $exception);
+    }
 
 }
