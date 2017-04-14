b70f59e27758315b325ecdc170090c9aaaa0a2a2
dereuromark <euromark@web.de>
2017-04-14 21:41:42 +0200

    Add MultiColumnAuthenticate

---
+++ tests/Fixture/MultiColumnUsersFixture.php
@@ -0,0 +1,71 @@
+<?php
+namespace Tools\Test\Fixture;
+
+use Cake\TestSuite\Fixture\TestFixture;
+
+class MultiColumnUsersFixture extends TestFixture {
+
+	/**
+	 * fields property
+	 *
+	 * @var array
+	 */
+	public $fields = [
+		'id' => ['type' => 'integer'],
+		'user_name' => ['type' => 'string', 'null' => false],
+		'email' => ['type' => 'string', 'null' => false],
+		'password' => ['type' => 'string', 'null' => false],
+		'token' => ['type' => 'string', 'null' => false],
+		'created' => 'datetime',
+		'updated' => 'datetime',
+		'_constraints' => ['primary' => ['type' => 'primary', 'columns' => ['id']]]
+	];
+
+	/**
+	 * records property
+	 *
+	 * @var array
+	 */
+	public $records = [
+		[
+			'user_name' => 'mariano',
+			'email' => 'mariano@example.com',
+			'password' => '5f4dcc3b5aa765d61d8327deb882cf99',
+			'token' => '12345', 'created' => '2007-03-17 01:16:23',
+			'updated' => '2007-03-17 01:18:31'
+		],
+		[
+			'user_name' => 'nate',
+			'email' => 'nate@example.com',
+			'password' => '5f4dcc3b5aa765d61d8327deb882cf99',
+			'token' => '23456',
+			'created' => '2007-03-17 01:18:23',
+			'updated' => '2007-03-17 01:20:31'
+		],
+		[
+			'user_name' => 'larry',
+			'email' => 'larry@example.com',
+			'password' => '5f4dcc3b5aa765d61d8327deb882cf99',
+			'token' => '34567',
+			'created' => '2007-03-17 01:20:23',
+			'updated' => '2007-03-17 01:22:31'
+		],
+		[
+			'user_name' => 'garrett',
+			'email' => 'garrett@example.com',
+			'password' => '5f4dcc3b5aa765d61d8327deb882cf99',
+			'token' => '45678',
+			'created' => '2007-03-17 01:22:23',
+			'updated' => '2007-03-17 01:24:31'
+		],
+		[
+			'user_name' => 'chartjes',
+			'email' => 'chartjes@example.com',
+			'password' => '5f4dcc3b5aa765d61d8327deb882cf99',
+			'token' => '56789',
+			'created' => '2007-03-17 01:22:23',
+			'updated' => '2007-03-17 01:24:31'
+		]
+	];
+
+}
---
+++ tests/TestCase/Auth/MultiColumnAuthenticateTest.php
@@ -0,0 +1,124 @@
+<?php
+namespace Tools\Test\TestCase\Auth;
+
+use Cake\I18n\Time;
+use Cake\Network\Request;
+use Cake\ORM\TableRegistry;
+use Cake\TestSuite\TestCase;
+use Tools\Auth\MultiColumnAuthenticate;
+
+class MultiColumnAuthenticateTest extends TestCase {
+
+	/**
+	 * @var array
+	 */
+	public $fixtures = ['plugin.Tools.multi_column_users'];
+
+	/**
+	 * @var \Tools\Auth\MultiColumnAuthenticate
+	 */
+	protected $auth;
+
+	/**
+	 * @var \Cake\Http\Response
+	 */
+	protected $response;
+
+	/**
+	 * @return void
+	 */
+	public function setUp() {
+		parent::setUp();
+
+		$this->Registry = $this->getMockBuilder('Cake\Controller\ComponentRegistry')->getMock();
+		$this->auth = new MultiColumnAuthenticate($this->Registry, [
+			'fields' => ['username' => 'user_name', 'password' => 'password'],
+			'userModel' => 'MultiColumnUsers',
+			'columns' => ['user_name', 'email']
+		]);
+
+		$password = password_hash('password', PASSWORD_DEFAULT);
+		$MultiColumnUsers = TableRegistry::get('MultiColumnUsers');
+		$MultiColumnUsers->updateAll(['password' => $password], []);
+
+		$this->response = $this->getMockBuilder('Cake\Http\Response')->getMock();
+	}
+
+	/**
+	 * @return void
+	 */
+	public function testAuthenticateEmailOrUsername() {
+		$request = new Request('posts/index');
+		$expected = [
+			'id' => 1,
+			'user_name' => 'mariano',
+			'email' => 'mariano@example.com',
+			'token' => '12345',
+			'created' => new Time('2007-03-17 01:16:23'),
+			'updated' => new Time('2007-03-17 01:18:31')
+		];
+
+		$request->data = [
+			'user_name' => 'mariano',
+			'password' => 'password'
+		];
+		$result = $this->auth->authenticate($request, $this->response);
+		$this->assertEquals($expected, $result);
+
+		$request->data = [
+			'user_name' => 'mariano@example.com',
+			'password' => 'password'
+		];
+		$result = $this->auth->authenticate($request, $this->response);
+		$this->assertEquals($expected, $result);
+	}
+	/**
+	 * @return void
+	 */
+	public function testAuthenticateNoUsername() {
+		$request = new Request('posts/index');
+		$request->data = ['password' => 'foobar'];
+		$this->assertFalse($this->auth->authenticate($request, $this->response));
+	}
+
+	/**
+	 * @return void
+	 */
+	public function testAuthenticateNoPassword() {
+		$request = new Request('posts/index');
+		$request->data = ['user_name' => 'mariano'];
+		$this->assertFalse($this->auth->authenticate($request, $this->response));
+
+		$request->data = ['user_name' => 'mariano@example.com'];
+		$this->assertFalse($this->auth->authenticate($request, $this->response));
+	}
+
+	/**
+	 * @return void
+	 */
+	public function testAuthenticateInjection() {
+		$request = new Request('posts/index');
+		$request->data = [
+			'user_name' => '> 1',
+			'password' => "' OR 1 = 1"
+		];
+		$this->assertFalse($this->auth->authenticate($request, $this->response));
+	}
+
+	/**
+	 * test scope failure.
+	 *
+	 * @return void
+	 */
+	public function testAuthenticateScopeFail() {
+		$this->auth->config('scope', ['user_name' => 'nate']);
+		$request = new Request('posts/index');
+		$request->data = [
+			'user_name' => 'mariano',
+			'password' => 'password'
+		];
+
+		$this->assertFalse($this->auth->authenticate($request, $this->response));
+	}
+
+}
