33bb575cb5a245a5ce108b4187f799dd573e30d9
mscherer <mark.scherer@spryker.com>
2018-07-11 11:21:16 +0200

    Disable error handler middleware by default.

--- src/TestSuite/IntegrationTestCase.php
+++ src/TestSuite/IntegrationTestCase.php
@@ -11,4 +11,26 @@ abstract class IntegrationTestCase extends CakeIntegrationTestCase {
 
 	use ToolsTestTrait;
 
+	/**
+	 * Globally disabling error handler middleware to see the actual errors instead of cloaking.
+	 *
+	 * Disable this when you explicitly test exception handling for controllers.
+	 *
+	 * @var bool
+	 */
+	protected $disableErrorHandlerMiddleware = true;
+
+	/**
+	 * @return void
+	 */
+	public function setUp()
+	{
+		parent::setUp();
+
+		if (!$this->disableErrorHandlerMiddleware) {
+			return;
+		}
+		$this->disableErrorHandlerMiddleware();
+	}
+
 }
