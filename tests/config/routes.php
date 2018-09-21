c535ce00af4a8f1aae359032aa9a1e7f6efb44a0
Mark Sch <dereuromark@users.noreply.github.com>
2018-09-21 17:46:41 +0200

    Update routes.php

--- tests/config/routes.php
+++ tests/config/routes.php
@@ -4,10 +4,7 @@ namespace Tools\Test\App\Config;
 
 use Cake\Routing\Router;
 
-//Router::extensions(['rss']);
-
 Router::scope('/', function($routes) {
-	//$routes->extensions(['rss']);
 	$routes->connect('/:controller', ['action' => 'index'], ['routeClass' => 'InflectedRoute']);
 	$routes->connect('/:controller/:action/*', [], ['routeClass' => 'InflectedRoute']);
 });
