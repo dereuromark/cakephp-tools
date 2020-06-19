6d11b17b91610fc9a183233259bd790f9557cfea
Ishan Vyas <ishan@pm.biztechconsultancy.com>
2020-06-19 17:17:34 +0530

    Add tests/TestCase/Utility/LogTest.php

---
+++ tests/TestCase/Utility/LogTest.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace Tools\Test\Utility;
+
+use Tools\TestSuite\TestCase;
+use Tools\Utility\Log;
+
+/**
+ * LogTest class
+ */
+class LogTest extends TestCase {
+	/**
+	 * File path to store log file.
+	 *
+	 * @var string
+	 */
+	private const CUSTOM_FILE_PATH = LOGS . 'my_file.log';
+
+	/**
+	 * setUp method
+	 *
+	 * @return void
+	 */
+	public function setUp() {
+		parent::setUp();
+	}
+
+	/**
+	 * testLogsStringData method
+	 *
+	 * @return void
+	 */
+	public function testLogsStringData() {
+		if (file_exists(self::CUSTOM_FILE_PATH)) {
+            unlink(self::CUSTOM_FILE_PATH);
+        }
+
+		$result = Log::write('It works!', 'my_file');
+
+		$this->assertTrue($result);
+		$this->assertFileExists(self::CUSTOM_FILE_PATH);
+
+		unlink(self::CUSTOM_FILE_PATH);
+	}
+}
