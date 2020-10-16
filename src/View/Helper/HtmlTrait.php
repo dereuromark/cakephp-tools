87ea3255fc99055bea1671bfac0cb5d88f8946ce
mscherer <mark.scherer@spryker.com>
2020-10-16 13:54:36 +0200

    Use traits

---
+++ src/View/Helper/HtmlTrait.php
@@ -0,0 +1,88 @@
+<?php
+
+namespace Tools\View\Helper;
+
+trait HtmlTrait {
+
+	/**
+	 * Display image tag from blob content.
+	 * Enhancement for HtmlHelper. Defaults to png image
+	 *
+	 * Options:
+	 * - type: png, gif, jpg, ...
+	 *
+	 * @param string $content Data in binary form
+	 * @param array $options Attributes
+	 * @return string HTML image tag
+	 */
+	public function imageFromBlob($content, array $options = []) {
+		$options += ['type' => 'png'];
+		$mimeType = 'image/' . $options['type'];
+
+		$text = 'data:' . $mimeType . ';base64,' . base64_encode($content);
+
+		return $this->formatTemplate('image', [
+			'url' => $text,
+			'attrs' => $this->templater()->formatAttributes($options, ['block', 'link']),
+		]);
+	}
+
+	/**
+	 * Creates a reset HTML link.
+	 * The prefix and plugin params are resetting to default false.
+	 *
+	 * ### Options
+	 *
+	 * - `escape` Set to false to disable escaping of title and attributes.
+	 * - `escapeTitle` Set to false to disable escaping of title. Takes precedence
+	 *   over value of `escape`)
+	 * - `confirm` JavaScript confirmation message.
+	 *
+	 * @param string $title The content to be wrapped by <a> tags.
+	 * @param string|array|null $url URL or array of URL parameters, or
+	 *   external URL (starts with http://)
+	 * @param array $options Array of options and HTML attributes.
+	 * @return string An `<a />` element.
+	 */
+	public function linkReset($title, $url = null, array $options = []) {
+		if (is_array($url)) {
+			$url += ['prefix' => false, 'plugin' => false];
+		}
+
+		return parent::link($title, $url, $options);
+	}
+
+	/**
+	 * Keep query string params for pagination/filter for this link,
+	 * e.g. after edit action.
+	 *
+	 * ### Options
+	 *
+	 * - `escape` Set to false to disable escaping of title and attributes.
+	 * - `escapeTitle` Set to false to disable escaping of title. Takes precedence
+	 *   over value of `escape`)
+	 * - `confirm` JavaScript confirmation message.
+	 *
+	 * @param string $title The content to be wrapped by <a> tags.
+	 * @param string|array|null $url URL or array of URL parameters, or
+	 *   external URL (starts with http://)
+	 * @param array $options Array of options and HTML attributes.
+	 * @return string An `<a />` element.
+	 * @return string Link
+	 */
+	public function linkComplete($title, $url = null, array $options = []) {
+		if (is_array($url)) {
+			// Add query strings
+			if (!isset($url['?'])) {
+				$url['?'] = [];
+			}
+			$url['?'] += $this->_View->getRequest()->getQuery();
+
+			$pass = $this->_View->getRequest()->getParam('pass');
+			$url = array_merge($url, $pass);
+		}
+
+		return parent::link($title, $url, $options);
+	}
+
+}
---
+++ src/View/Helper/UrlTrait.php
@@ -0,0 +1,99 @@
+<?php
+
+namespace Tools\View\Helper;
+
+trait UrlTrait {
+
+	/**
+	 * @param array $url
+	 * @return array
+	 */
+	public function resetArray(array $url): array {
+		$url += $this->defaults();
+
+		return $url;
+	}
+
+	/**
+	 * @param array $url
+	 * @return array
+	 */
+	public function completeArray(array $url): array {
+		$url = $this->addQueryStrings($url);
+		$url = $this->addPassed($url);
+
+		return $url;
+	}
+
+	/**
+	 * Creates a reset URL.
+	 * The prefix and plugin params are resetting to default false.
+	 *
+	 * Can only add defaults for array URLs.
+	 *
+	 * @param string|array|null $url URL.
+	 * @param array $options
+	 * @return string Full translated URL with base path.
+	 */
+	public function buildReset($url, array $options = []): string {
+		if (is_array($url)) {
+			$url += $this->defaults();
+		}
+
+		return $this->build($url, $options);
+	}
+
+	/**
+	 * Returns a URL based on provided parameters.
+	 *
+	 * Can only add query strings for array URLs.
+	 *
+	 * @param string|array|null $url URL.
+	 * @param array $options
+	 * @return string Full translated URL with base path.
+	 */
+	public function buildComplete($url, array $options = []): string {
+		if (is_array($url)) {
+			$url = $this->addQueryStrings($url);
+		}
+
+		return $this->build($url, $options);
+	}
+
+	/**
+	 * @return array
+	 */
+	public function defaults(): array {
+		return [
+			'prefix' => false,
+			'plugin' => false,
+		];
+	}
+
+	/**
+	 * @param array $url
+	 *
+	 * @return array
+	 */
+	protected function addQueryStrings(array $url): array {
+		if (!isset($url['?'])) {
+			$url['?'] = [];
+		}
+		$url['?'] += $this->_View->getRequest()->getQuery();
+
+		return $url;
+	}
+
+	/**
+	 * @param array $url
+	 *
+	 * @return array
+	 */
+	protected function addPassed(array $url) {
+		$pass = $this->_View->getRequest()->getParam('pass');
+		$url = array_merge($url, $pass);
+
+		return $url;
+	}
+
+}
