4bfb462c0fbc36bfacea139846ba8b2ce9a4db93
Kevin Pfeifer <kevin.pfeifer@sunlime.at>
2023-01-27 08:34:05 +0100

    fix json datatype

--- tests/test_app/Model/Table/DataTable.php
+++ tests/test_app/Model/Table/DataTable.php
@@ -2,21 +2,18 @@
 
 namespace TestApp\Model\Table;
 
-use Cake\Database\Schema\TableSchemaInterface;
 use Tools\Model\Table\Table;
 
 class DataTable extends Table {
 
-	/**
-	 * @param \Cake\Database\Schema\TableSchemaInterface $schema
-	 *
-	 * @return \Cake\Database\Schema\TableSchemaInterface
-	 */
-	protected function _initializeSchema(TableSchemaInterface $schema): TableSchemaInterface {
+  /**
+   * @param array $config
+   * @return void
+   */
+	public function initialize(array $config): void {
+		$schema = $this->getSchema();
 		$schema->setColumnType('data_json', 'json');
 		$schema->setColumnType('data_array', 'array');
-
-		return $schema;
 	}
 
 }
