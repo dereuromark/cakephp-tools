25eb3e9d71e05a0fabdff04d26e622ca92ddf0ca
Kevin Pfeifer <kevin.pfeifer@sunlime.at>
2023-07-08 19:24:44 +0200

    fix DataTable setColumnType

--- tests/test_app/Model/Table/DataTable.php
+++ tests/test_app/Model/Table/DataTable.php
@@ -2,21 +2,16 @@
 
 namespace TestApp\Model\Table;
 
-use Cake\Database\Schema\TableSchemaInterface;
 use Tools\Model\Table\Table;
 
 class DataTable extends Table {
 
-	/**
-	 * @param \Cake\Database\Schema\TableSchemaInterface $schema
-	 *
-	 * @return \Cake\Database\Schema\TableSchemaInterface
-	 */
-	protected function _initializeSchema(TableSchemaInterface $schema): TableSchemaInterface {
-		$schema->setColumnType('data_json', 'json');
-		$schema->setColumnType('data_array', 'array');
+	public function initialize(array $config): void
+	{
+		parent::initialize($config);
 
-		return $schema;
+		$schema = $this->getSchema();
+		$schema->setColumnType('data_json', 'json');
 	}
 
 }
